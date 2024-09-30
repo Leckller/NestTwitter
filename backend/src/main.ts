6a7365a378f9302f61876dc1cbc0c81eca69da0b
Gusttavo Ruy <7leckler@gmail.com>
2024-09-30 19:28:20 -0300

    feat: adicionei o swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,22 @@ async function bootstrap() {
   // Configuração para validar com DTOs
   app.useGlobalPipes(new ValidationPipe());
 
+  // Swagger Config
+  const config = new DocumentBuilder()
+  .setTitle('Twitter Example')
+  .setDescription('Api baseada no Twitter')
+  .setVersion('2.3')
+  .addTag('Funny')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
+  
+  console.log("\nNest Backend:")
+  console.log("\x1b[36m%s\x1b[0m", "http://localhost:3000")
+  console.log("\nSwagger:")
+  console.log("\x1b[36m%s\x1b[0m", "http://localhost:3000/api")
 }
 bootstrap();
