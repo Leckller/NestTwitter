dfddc147db3ded35323e76c05175fcad9df4c235
Gusttavo Ruy <7leckler@gmail.com>
2025-01-12 23:09:59 -0300

    fix: consertei o css

--- frontend/src/pages/Profile/Styles/StyledEditProfile.ts
+++ frontend/src/pages/Profile/Styles/StyledEditProfile.ts
@@ -2,10 +2,9 @@ import styled from "styled-components";
 
 export const StyledEditProfile = styled.div`
   position: absolute;
-  display:flex;
+  display: flex;
   flex-direction: column;
   align-items: center;
-  display: flex;
   height: 100%;
   width: 100%;
 
@@ -20,21 +19,21 @@ export const StyledEditProfile = styled.div`
 
   section {
     position: absolute;
-    background-color: blue;
-    top: 0;
-    max-width: 600px;
-    overflow-x: hidden;
     background-color: white;
+    max-width: 600px;
     z-index: 20;
     width: 80%;
     height: 80%;
     max-height: 80vh;
     min-height: 400px;
-    margin-top: 20%;
+    margin-top: 50px;
 
     button {
       img {
         width: 100%;
+        &:hover {
+          backdrop-filter: blur(10px);
+        }
       }
     }
 
